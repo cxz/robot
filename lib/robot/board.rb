5b9ad3399ad4afd3867de26196dd0145ca8e9a50
amaia <alexandre.maia@gmail.com>
2013-10-11 21:26:38 +0000

    bord#contains to use board's SIZE

--- lib/robot/board.rb
+++ lib/robot/board.rb
@@ -1,6 +1,6 @@
 class Board
   SIZE = 5
   def contains? position
-    position.is_valid? and position.x >= 0 and position.x <= 4 and position.y >= 0 and position.y <= 4
+    position.is_valid? and position.x >= 0 and position.x < SIZE and position.y >= 0 and position.y < SIZE
   end
 end
