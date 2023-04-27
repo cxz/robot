10eb1a10db6b3f012b1f7c61dbb167d54607be4f
amaia <alexandre.maia@gmail.com>
2023-04-27 18:19:59 +0200

    cleanup

--- lib/robot/command.rb
+++ lib/robot/command.rb
@@ -3,16 +3,23 @@
 class Command
   class << self
     def inherited(base)
+      super
+
       @registry ||= []
       @registry << base
     end
 
     def get_klass(name)
       @klass_cache ||= {}
-      @klass_cache[name] ||=
-        @registry.find do |klass|
-          klass.const_get(:NAME) == name || (klass.const_defined?(:ALIAS) && klass.const_get(:ALIAS) == name)
-        end
+      @klass_cache[name] ||= @registry.find { |klass| [klass.name, klass.alias].include?(name) }
+    end
+
+    def name
+      const_get(:NAME)
+    end
+
+    def alias
+      const_get(:ALIAS) if const_defined?(:ALIAS)
     end
   end
 
